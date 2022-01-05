fb4188523ddff40f1ae0f94084e192f8903077ca
OhRaekyu <dhforb1234@naver.com>
2022-01-05 15:49:36 +0900

    :memo: add : 파일추가(join_mebership.js, join_membership_2.js)

--- script/login_email.js
+++ script/login_email.js
@@ -1,31 +1,39 @@
 // 아이디, 비밀번호 유효성 검사 js
 const loginBtn = document.querySelector("#loginBtn");
-const inputID = document.querySelector("#inpId");
+const inputEmail = document.querySelector("#inpEmail");
 const inputPW = document.querySelector("#inpPw");
 const msgError = document.querySelector("#msgError");
 
 // 유저 정보 예시
 const userData = [
     {
-        userID : "asd@naver.com",
-        userPW : "1234"
+        userEmail : "asd@naver.com",
+        userPW : "1234567",
+        userID : "asd123",
+        userName : "오래규",
+        userIntro : "안녕하세요 저는 000입니다.",
+        followCount : 0,
     },
     {
-        userID : "qwe@gmail.com",
-        userPW : "1234"
+        userEmail : "qwe@naver.com",
+        userPW : "1234567",
+        userID : "qwe456",
+        userName : "홍길동",
+        userIntro : "안녕하세요 저는 000입니다.",
+        followCount : 10,
     }
 ];
 
-// 아이디, 비밀번호가 모두 입력되면 버튼 활성화 함수
+// 이메일, 비밀번호가 모두 입력되면 버튼 활성화 함수
 function inputCheck() {
-    if (inputID.value && inputPW.value) {
+    if (inputEmail.value && inputPW.value) {
         loginBtn.classList.remove("disabled");
     } else {
         loginBtn.classList.add("disabled");
     }
 };
 
-inputID.addEventListener("keyup", inputCheck);
+inputEmail.addEventListener("keyup", inputCheck);
 inputPW.addEventListener("keyup", inputCheck);
 
 
@@ -36,8 +44,8 @@ function emailCheck(email) {
 };
 
 // 유효성 검사 함수
-function validationCheck(id, pw) {
-    const userCheck = userData.find(val => val.userID === id);
+function validationCheck(email, pw) {
+    const userCheck = userData.find(val => val.userEmail === email);
 
     if (userCheck) {
         return userCheck.userPW === pw;
@@ -47,16 +55,26 @@ function validationCheck(id, pw) {
 }
 
 loginBtn.addEventListener("click", function() {
-    const inputUserID = inputID.value;
+    const inputUserEmail = inputEmail.value;
     const inputUserPW = inputPW.value;
-    const isEmail = emailCheck(inputUserID);
-    const isValid = validationCheck(inputUserID, inputUserPW);
+    const isEmail = emailCheck(inputUserEmail);
+    const isValid = validationCheck(inputUserEmail, inputUserPW);
 
     if (isEmail) {
         // 이메일 형식 검사 성공
+        console.log("이메일 형식 검사 성공");
         if (isValid) {
             // 유효성 검사 성공
-            location.href ="./home.html";
+            console.log("유효성 검사 성공");
+            if (userData.find(ele => ele.userEmail === inputUserEmail).followCount > 0) {
+                // 팔로우가 있을 때
+                console.log("팔로우가 있을 때");
+                location.href ="./home_post.html";
+            } else {
+                // 팔로우가 없을 때
+                console.log("팔로우가 없을 때");
+                location.href ="./home.html";
+            }
         } else {
             // 유효성 검사 실패
             msgError.classList.add("error");
